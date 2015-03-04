fc7a3baba4739768c0fca06af2dd26caff911905
Sebastian Pölsterl <sebp@k-d-w.org>
2015-03-04 14:58:11 +0100

    ncurses: Compile with --with-termlib=tinfo

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -3,9 +3,8 @@
 mkdir $PREFIX/lib
 
 sh ./configure --prefix=$PREFIX \
-    --without-debug --without-ada --without-manpages \
-    --with-shared --disable-overwrite
-
+    --without-debug --without-ada --without-manpages --without-progs \
+    --with-shared --disable-overwrite --with-termlib=tinfo --enable-widec
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
